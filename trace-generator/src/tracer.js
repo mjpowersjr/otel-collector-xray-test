e0075958b2bbb9dbcf8343e9256534a484703aba
Mike Powers <mjpowersjr@gmail.com>
2021-01-08 08:22:54 -0500

    Add GRPC exporter support

--- trace-generator/src/tracer.js
+++ trace-generator/src/tracer.js
@@ -1,13 +1,19 @@
 // const { SimpleSpanProcessor } = require("@opentelemetry/tracing");
 const { BatchSpanProcessor } = require("@opentelemetry/tracing");
 const { NodeTracerProvider } = require('@opentelemetry/node');
-// const { CollectorTraceExporter } = require('@opentelemetry/exporter-collector-grpc');
-const { CollectorTraceExporter } = require('@opentelemetry/exporter-collector');
+const { CollectorTraceExporter: CollectorTraceExporterGrpc } = require('@opentelemetry/exporter-collector-grpc');
+const { CollectorTraceExporter: CollectorTraceExporterHttp } = require('@opentelemetry/exporter-collector');
 const { AWSXRayPropagator } = require('@aws/otel-aws-xray-propagator');
 const { AwsXRayIdGenerator } = require('@aws/otel-aws-xray-id-generator');
 const { propagation, trace } = require("@opentelemetry/api");
 
-const otelCollectorEndpoint = process.env.OTEL_COLLECTOR
+// Use HTTP
+const otelCollectorEndpoint = process.env.OTEL_COLLECTOR_HTTP;
+const CollectorTraceExporter = CollectorTraceExporterHttp;
+
+// Use GRPC
+// const otelCollectorEndpoint = process.env.OTEL_COLLECTOR_GRPC;
+// const CollectorTraceExporter = CollectorTraceExporterGrpc;
 
 module.exports = (serviceName) => {
     propagation.setGlobalPropagator(new AWSXRayPropagator());
@@ -18,13 +24,17 @@ module.exports = (serviceName) => {
         resources: {},
     });
 
-    console.log(`settings up otlpExporter(url: ${otelCollectorEndpoint})`)
+    console.log(`settings up ${CollectorTraceExporter.name}(url: ${otelCollectorEndpoint})`)
     const otlpExporter = new CollectorTraceExporter({
         serviceName,
         url: otelCollectorEndpoint,
-        protocolNode: 2,
+        // protocolNode: 2,
+        // httpAgentOptions: {
+        //     keepAlive: false
+        // }
         // logger: console
     });
+
     tracerProvider.addSpanProcessor(new BatchSpanProcessor(otlpExporter));
     // tracerProvider.addSpanProcessor(new SimpleSpanProcessor(otlpExporter));
 
